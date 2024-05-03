5a1edf57e31174215476a160f43e007e353a55ac
yogeshkumarsaini <yogeshkumarsaini1085@gmail.com>
2024-05-03 15:20:07 +0530

    task commit

---
+++ float1.java
@@ -0,0 +1,8 @@
+class float1{
+    public static void main(String[] args) {
+        float s=2.456f;
+        System.out.printf("%.2f\n",s);
+        double d = 123.345664;
+        System.out.printf("%.2f",d);
+    }
+}
